9d82d7f9841d74cbd867c1ed79b0aa35714a23e6
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-07-14 19:39:37 +0300

    Like, Bookmarks, Rating

--- store/models.py
+++ store/models.py
@@ -6,7 +6,30 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_books')
+    # явно связываем отношение ManyToMany через нашу таблицу, чтобы добавить в неё дополнительные поля
+    # так как у нас дво поля связанны с User, возникает конфликт при обращении от юзера к книгам,
+    # user.book (то ли прочитал, то ли написал), для этого прописываем related_name, по умолчанию related_name=book_set()
+    # на к двум полям нельзя применить одинаковое название, для одной из связей нужно изменить название на своё
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books')
 
     def __str__(self):
         return f"Id {self.id}: {self.name}"
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Ok'),
+        (2, 'Fine'),
+        (3, 'Good'),
+        (4, 'Amazing'),
+        (5, 'Incredible')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.book.name}, RATE {self.rate}'
