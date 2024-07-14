4cf1efe1152e4001b53b86b473b1338f5292adc7
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-07-14 11:07:54 +0300

    CRUD

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -8,11 +11,20 @@ from store.serializers import BookSerializer
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
+
         self.book_1 = Book.objects.create(name='Test book 1', author_name='Author 1', price=25)
         self.book_2 = Book.objects.create(name='Test book 2', author_name='Author 5', price=75)
         self.book_3 = Book.objects.create(name='Test book Author 1', author_name='Author 2', price=55)
 
-    def test_get(self):
+    def test_get_detail(self):
+        url = reverse('book-detail', args=(self.book_2.id,))
+        response = self.client.get(url)
+        serializer_data = BookSerializer(self.book_2).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_list(self):
         # возвращает маршрут с именем book-list
         url = reverse('book-list')
         print(url)
@@ -47,3 +59,53 @@ class BooksApiTestCase(APITestCase):
         serializer_data = BookSerializer([self.book_2, self.book_3, self.book_1], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        # проверяем количество книг перед созданием книги
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {
+            'name': 'Programing in Python 3',
+            'price': 150,
+            'author_name': 'Mark Summerfield'
+        }
+        # преобразуем словарь в json строку
+        json_data = json.dumps(data)
+
+        # авторизуемся с помощю созданнго юзера
+        self.client.force_login(self.user)
+
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            'name': self.book_1.name,
+            'price': 575,
+            'author_name': self.book_1.author_name
+        }
+        # преобразуем словарь в json строку
+        json_data = json.dumps(data)
+
+        # авторизуемся с помощю созданнго юзера
+        self.client.force_login(self.user)
+
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        # обновляем запись из дб
+        self.book_1.refresh_from_db()
+        self.assertEqual(575, self.book_1.price)
+
+    def test_delete(self):
+        url = reverse('book-detail', args=(self.book_3.id,))
+
+        # для всех манипуляций с обьектами нужно быть авторизованным
+        self.client.force_login(self.user)
+
+        response = self.client.delete(url)
+        exist = Book.objects.filter(id=self.book_3.id).exists()
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(False, exist)
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
@@ -18,7 +18,7 @@ class BookViewSet(ModelViewSet):
     search_fields = ['name', 'author_name']       # ?search=
     ordering_fields = ['price', 'author_name']    # ?ordering=   price   -price
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 def auth(request):
     return render(request, 'oauth.html')
